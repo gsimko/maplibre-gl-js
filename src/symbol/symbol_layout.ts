00712c09f4d024879f692637f3d822f08f7e64bd
Gabor Simko <gsimko3@gmail.com>
2024-02-13 02:33:02 -0700

    remove simplification

--- src/symbol/symbol_layout.ts
+++ src/symbol/symbol_layout.ts
@@ -348,7 +348,7 @@ function addFeature(bucket: SymbolBucket,
     if (symbolPlacement === 'line') {
         for (const line of clipLine(feature.geometry, 0, 0, EXTENT, EXTENT)) {
             // simplify the line geometry to not block text-max-angle
-            const sline = simplify(line, EXTENT / 128).map(x=>new Point(x.x, x.y));
+            const sline = line;  // simplify(line, EXTENT / 128).map(x=>new Point(x.x, x.y));
             const anchors = getAnchors(
                 sline,
                 symbolMinDistance,
